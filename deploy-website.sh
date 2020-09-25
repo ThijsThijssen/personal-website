0cacab3391e809d6c43db27caf51be31fe1c5014
ThijsThijssen <thijsthijssen@outlook.com>
2020-09-25 18:39:29 +0200

    Deployed website on: Fri 25 Sep 2020 18:39:29 CEST

--- deploy-website.sh
+++ deploy-website.sh
@@ -1,30 +1,45 @@
-# build for production
+# Check if git branch is master
 echo "------------------------------"
-echo "Local: Building for production..."
+echo "Local: Checking for master branch..."
 echo "------------------------------"
-ng build --prod
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+masterBranch="master"
+if [ $currentBranch == $masterBranch ]
+then
+    # build for production
+    echo "------------------------------"
+    echo "Local: Building for production..."
+    echo "------------------------------"
+    ng build --prod
 
-# add production files
-echo "------------------------------"
-echo "Local: Adding production files to local git repository..."
-echo "------------------------------"
-git add .
+    # add production files
+    echo "------------------------------"
+    echo "Local: Adding production files to local git repository..."
+    echo "------------------------------"
+    git add .
 
-# commit production files
-echo "------------------------------"
-echo "Local: Committing production files..."
-echo "------------------------------"
-currentDate=$(date)
-git commit -m "Deployed website on: $currentDate"
+    # commit production files
+    echo "------------------------------"
+    echo "Local: Committing production files..."
+    echo "------------------------------"
+    currentDate=$(date)
+    git commit -m "Deployed website on: $currentDate"
 
-# push production files to remote repository
-echo "------------------------------"
-echo "Local: Pushing production files to remote repository..."
-echo "------------------------------"
-git push
+    # push production files to remote repository
+    echo "------------------------------"
+    echo "Local: Pushing production files to remote repository..."
+    echo "------------------------------"
+    git push
 
-# update website contents
-echo "------------------------------"
-echo "Local: Updating website contents..."
-echo "------------------------------"
-ssh -t thijsthijssen.nl@ssh.strato.de "./update-website.sh"
+    # update website contents
+    echo "------------------------------"
+    echo "Local: Updating website contents..."
+    echo "------------------------------"
+    ssh -t thijsthijssen.nl@ssh.strato.de "./update-website.sh"
+else
+    # wrong branch message
+    echo "------------------------------"
+    echo "Local: You can only deploy from the master branch..."
+    echo "------------------------------"
+fi
+
