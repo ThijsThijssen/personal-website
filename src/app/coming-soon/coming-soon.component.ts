5ae7c44c294efdc50426125075513ec7584ca2a3
ThijsThijssen <thijsthijssen@outlook.com>
2020-10-03 08:00:06 +0200

    Deployed website on: Sat Oct  3 08:00:06 CEST 2020

--- src/app/coming-soon/coming-soon.component.ts
+++ src/app/coming-soon/coming-soon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-coming-soon',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComingSoonComponent implements OnInit {
 
-  constructor() { }
+  innerWidth: any;
+  fontSizeBreakPoint;
+
+  constructor() {
+    this.fontSizeBreakPoint = 500;
+  }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = event.target.innerWidth;
   }
 
   goToLink(url: string) {
